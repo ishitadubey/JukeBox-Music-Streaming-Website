ace6867b484f286260e1c8172c2417a91886a5cc
ishitadubey <54002608+ishitadubey@users.noreply.github.com>
2021-06-02 20:22:41 +0530

    Update changepass.php

--- sidebar_links/changepass.php
+++ sidebar_links/changepass.php
@@ -1,6 +1,15 @@
 <html>
 <head>
-<title> Change Password </title>
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <!-- APlayer CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aplayer/1.10.1/APlayer.min.css"/>
+
+    <title>Change Password</title>
+    <link rel="icon" href="headphones.png" type="image/png">
 <style>
 body{
             background-color: #f7f7f7;
