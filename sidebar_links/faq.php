2663a5cc26827d647ead302f1647cf67fb34f630
ishitadubey <54002608+ishitadubey@users.noreply.github.com>
2021-06-02 20:22:13 +0530

    Update faq.php

--- sidebar_links/faq.php
+++ sidebar_links/faq.php
@@ -1,10 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <!-- APlayer CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aplayer/1.10.1/APlayer.min.css"/>
+
+    <title>MusicStream-FAQs</title>
+    <link rel="icon" href="headphones.png" type="image/png">
     <style>
         body{
             height: 100%;
