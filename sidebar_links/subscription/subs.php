145aae1c36e897e7b174a02a6b0d3a336f1c9aa6
ishitadubey <54002608+ishitadubey@users.noreply.github.com>
2021-06-02 20:24:45 +0530

    Update subs.php

--- sidebar_links/subscription/subs.php
+++ sidebar_links/subscription/subs.php
@@ -1,7 +1,15 @@
 <!DOCTYPE html>
 <head>
-<link rel="stylesheet" href="external2.css">
-<title>19BIT0324</title>
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <!-- APlayer CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aplayer/1.10.1/APlayer.min.css"/>
+
+    <title>Subscribe</title>
+    <link rel="icon" href="headphones.png" type="image/png">
 
 <style>
 body,h1{
