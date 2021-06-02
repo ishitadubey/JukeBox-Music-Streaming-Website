21c017605014d2a3c1962c2b92d48c21212185df
ishitadubey <54002608+ishitadubey@users.noreply.github.com>
2021-06-02 19:39:01 +0530

    Add files via upload

---
+++ sidebar_links/faq.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body{
+            height: 100%;
+            background-repeat: no-repeat;
+
+        
+  
+  background: linear-gradient(#cc99ff,#99ccff);
+        }
+        #got{
+            font-family: 'Trebuchet MS', sans-serif;
+            padding-top: 100px;
+            padding-bottom: 35px;
+            text-align: center;
+            justify-content: center;
+            font-size: xx-large;
+            color:black;
+            font-weight: bolder;
+        }
+        .now{
+            padding-left: 20px;
+            font-family: 'Trebuchet MS', sans-serif;
+            font-size: x-large;
+            color:black;
+            
+            
+
+        }
+  </style>
+
+  <body>
+  <div><div id="got">FREQUENTLY ASKED QUESTIONS..</div></div>
+
+<div class="now">1.How can I search for songs, artists, playlists or albums?<br><br>
+   
+    <div>
+        ->You can search by entering your search text in the search bar at the top of the page.<br>
+        
+       
+    <div>
+        </div>
+        <br> 2.What languages is the App available in?<br><br>
+        ->The App display is currently available in English only. More languages to follow soon.
+
+    </div>
+    <div>
+       <br> 3.What all languages of Music is available?<br><br>
+        ->Along with English, our app offers songs in hindi and punjabi too.
+    </div>
+    <div>
+        <br> 4.How much does it cost to use the app?<br><br>
+       -> Our app is totally free.
+    </div>
+    <br> 5. Can we download songs from this app?<br><br>
+    -> No, right now you can only hear songs but we are working on the downloading feature too.
+    </div>
+    
+</div>
+
+
+    </style>
+</head>
+<body>
+    
+</body>
+</html>
