b799dafc86b81b095bda17a30d89bc78a80321b7
MUSKAN KASERA <70365874+muskan27-git@users.noreply.github.com>
2021-06-02 22:12:06 +0530

    Update faq.php

--- sidebar_links/faq.php
+++ sidebar_links/faq.php
@@ -1,79 +1,85 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-    <!-- APlayer CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aplayer/1.10.1/APlayer.min.css"/>
-
-    <title>MusicStream-FAQs</title>
-    <link rel="icon" href="headphones.png" type="image/png">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
     <style>
-        body{
-            height: 100%;
-            background-repeat: no-repeat;
+        body {
+
+background:url("https://www.smtusa.com/uploads/faqs.jpg") no-repeat center center fixed;
+-webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
 
         
-  
-  background: linear-gradient(#cc99ff,#99ccff);
-        }
-        #got{
+
+}
+        #got {
             font-family: 'Trebuchet MS', sans-serif;
-            padding-top: 100px;
+            padding-top: 60px;
             padding-bottom: 35px;
             text-align: center;
             justify-content: center;
             font-size: xx-large;
-            color:black;
+            color:blue;
             font-weight: bolder;
+            
         }
-        .now{
-            padding-left: 20px;
+
+        .now {
+            padding-left: 120px;
             font-family: 'Trebuchet MS', sans-serif;
             font-size: x-large;
             color:black;
-            
-            
+            font-weight: bolder;
+
+
+
 
         }
-  </style>
+    </style>
 
-  <body>
-  <div><div id="got">FREQUENTLY ASKED QUESTIONS..</div></div>
+<body>
+    <div>
+        <div id="got">FREQUENTLY ASKED QUESTIONS..</div>
+    </div>
 
-<div class="now">1.How can I search for songs, artists, playlists or albums?<br><br>
-   
-    <div>
-        ->You can search by entering your search text in the search bar at the top of the page.<br>
-        
-       
-    <div>
+    <div class="now">1.How can I search for songs, artists, playlists or albums?<br><br>
+
+        <div>
+            ->You can search by entering your search text in the search bar at the top of the page.<br>
+
+
+            <div>
+            </div>
+            <br> 2.What languages is the App available in?<br><br>
+            ->The App display is currently available in English only. More languages to follow soon.
+
         </div>
-        <br> 2.What languages is the App available in?<br><br>
-        ->The App display is currently available in English only. More languages to follow soon.
+        <div>
+            <br> 3.What all languages of Music is available?<br><br>
+            ->Along with English, our app offers songs in hindi and punjabi too.
+        </div>
+        <div>
+            <br> 4.How much does it cost to use the app?<br><br>
+            -> Our app is totally free.
+        </div>
+        <br> 5. Can we download songs from this app?<br><br>
+        -> No, right now you can only hear songs but we are working on the downloading feature too.
+    </div>
 
     </div>
-    <div>
-       <br> 3.What all languages of Music is available?<br><br>
-        ->Along with English, our app offers songs in hindi and punjabi too.
-    </div>
-    <div>
-        <br> 4.How much does it cost to use the app?<br><br>
-       -> Our app is totally free.
-    </div>
-    <br> 5. Can we download songs from this app?<br><br>
-    -> No, right now you can only hear songs but we are working on the downloading feature too.
-    </div>
-    
-</div>
 
 
     </style>
-</head>
-<body>
-    
-</body>
+    </head>
+
+    <body>
+
+    </body>
+
 </html>
