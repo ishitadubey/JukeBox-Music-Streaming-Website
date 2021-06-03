47541ed5ef811dcaa44ee925a567b13ab58cb780
ishitadubey <54002608+ishitadubey@users.noreply.github.com>
2021-06-03 21:48:39 +0530

    Update checkout.php

--- sidebar_links/subscription/checkout.php
+++ sidebar_links/subscription/checkout.php
@@ -2,15 +2,13 @@
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-    <!-- APlayer CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aplayer/1.10.1/APlayer.min.css"/>
+<script src="https://code.angularjs.org/1.4.0-rc.1/angular.js"></script>
 
-    <title>Checkout</title>
-    <link rel="icon" href="headphones.png" type="image/png">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <style>
-body {
+
+
+body{
   font-family: Arial;
   font-size: 17px;
   padding: 8px;
@@ -115,40 +113,93 @@ span.price {
 }
 </style>
 </head>
-<body>
+<body ng-app="validationApp" ng-controller="mainController">
 
 
 <div class="row"  style="padding:100px 300px;">
   <div class="col-50">
     <div class="container" >
-      <form  action="payscript.php" method="post" style="padding: 25px;">
+      <form  class="form"  action="payscript.php" method="post" style="padding: 25px;">
       
         <div class="row" >
           <div class="col-25">
             <h3 style="text-align: center;margin:20px 10px;font-family: lato;">Checkout Form</h3>
           
-
+            <div class="form-group" ng-class="{ 'has-error' : 
+             co.name.$invalid && !co.name.$pristine }">
             <label for="fname"><i class="fa fa-user"></i> Full Name</label>
-            <input type="text" id="fname" name="name" placeholder="John M. Doe">
+            <input type="text" id="fname" name="name" class="form-control" ng-model="name" 
+                   ng-minlength="3" ng-maxlength="14" required placeholder="John M. Doe">
+                   <p ng-show="co.name.$dirty && co.name.$error.required" 
+               class="help-block">Name is required</p>
+            <p ng-show="lco.name.$error.minlength" class="help-block">
+               Name is too short.
+            </p>
+            <p ng-show="co.name.$error.maxlength" class="help-block">
+              Name is too long.
+            </p>
+        </div>
+        <div class="form-group" ng-class="{ 'has-error' :co.userName.$invalid && !co.userName.$pristine }">
             <label for="email"><i class="fa fa-envelope"></i> Email</label>
-            <input type="text" id="email" name="email" placeholder="john@example.com">
+            <input type="text" id="email" name="email" placeholder="john@example.com" class="form-control" ng-model="email" ng-pattern="^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$">
+            <p 
+          ng-show="co.email.$invalid && !co.email.$pristine" 
+            class="help-block">
+              Enter a valid email.
+            </p>
+        </div>
             <input type="hidden" value="<?php echo 'OID'.rand(100,1000);?>" name="orderid">
             <input type="hidden" value="<?php echo 150;?>" name="amount">
-            <label for="city"><i class="fa fa-mobile"></i> Mobile</label>
-            <input type="text" id="city" name="mobile" placeholder="Mobile Number">
+            <div class="form-group" ng-class="{ 'has-error' : co.mobile.$invalid && !co.mobile.$pristine }">
+            <label for="phone"><i class="fa fa-mobile"></i> Mobile</label>
+            <input type="text" id="phone" name="mobile" placeholder="Mobile Number" class="form-control" ng-model="phone" ng-minlength="10" ng-maxlength="10" required>
+             <p ng-show="co.phone.$error.number && co.phone.$error.required" 
+               class="help-block">Address is required</p>
+            <p ng-show="((co.phone.$error.minlength || co.phone.$error.maxlength)&& co.ohone.$dirty)" class="help-block">
+               Phone should be of 10 digits
+            </p>
+			</div>
+            <div class="form-group" ng-class="{ 'has-error' : 
+             co.address.$invalid && !co.adress.$pristine }">  
             <label for="adr"><i class="fa fa-address-card-o"></i> Address</label>
-            <input type="text" id="adr" name="address" placeholder="542 W. 15th Street">
+            <input type="text" id="adr" name="address" placeholder="542 W. 15th Street" class="form-control" ng-model="address" 
+                   ng-minlength="10" ng-maxlength="40" required>
+                   <p ng-show="co.address.$dirty && co.address.$error.required" 
+               class="help-block">Address is required/p>
+            <p ng-show="co.adress.$error.minlength" class="help-block">
+               Address is too short.
+            </p>
+            <p ng-show="co.address.$error.maxlength" class="help-block">
+              Address is too long.
+            </p>         
 
 
           
         </div>
        
-        <input type="submit"  value="Pay Now" class="btn">
+        <button type="submit"  value="Pay Now" class="btn" ng-disabled="co.$invalid">
+          Pay Now
+        </button>
       </form>
-    </div>
   </div>
  
 </div>
+<script>
+    // create angular app
+var validationApp = angular.module('validationApp', []);
 
+// create angular controller
+validationApp.controller('mainController', function($scope) {
+
+    // function to submit the form after all validation has occurred            
+    $scope.submitForm = function(isValid) {
+
+        // check to make sure the form is completely valid
+        if (isValid) { 
+            alert('Login Form is valid');
+        }
+    };
+});
+    </script>
 </body>
 </html>
